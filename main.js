ae3909ba0695fd414d49f741b8864acc955c9b15
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-22 22:03:45 +0530

    JS-for-natural-num

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let a = 7;
+sum = 0
+document.write("The first 7 natural number is : ")
+for (i = 0; i <= a; i++) {
+    document.write(i + " ");
+    sum = sum + i
+}
+document.write("<br>")
+document.write("The Sum of Natural Number upto 7 terms : " + sum)
+
+
+
+
+
+
+
